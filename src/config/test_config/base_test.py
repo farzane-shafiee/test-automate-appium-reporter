1824ed79f40cd419ecb6ecb84a623a9941b06016
farzaneh <lilylotus68@gmail.com>
2023-07-05 15:44:10 +0330

    add initialize_mysql_manager

--- src/config/test_config/base_test.py
+++ src/config/test_config/base_test.py
@@ -1,20 +1,27 @@
 import json
+import os
+from dotenv import load_dotenv
 from appium import webdriver
 import yaml
 from selenium.webdriver.support.wait import WebDriverWait
 # from selenium.common.exceptions import ConnectionRefusedError
+from src.db_connection_handler.db_handler import MySQLManager
 from src.logs.logs_config import logger
 
 
 class BaseTest:
     driver = None
     wait = None
+    mysql_manager = None
 
     @classmethod
     def setup_class(cls):
         """
         Remote and connect to device_data.
         """
+        load_dotenv()
+        cls.initialize_mysql_manager()
+
         try:
             cls.driver = webdriver.Remote("http://localhost:4723/wd/hub", cls.read_data_device())
         except Exception as e:
@@ -27,6 +34,30 @@ class BaseTest:
     # def teardown_class(cls):
     #     cls.driver.close_app()
 
+    @classmethod
+    def initialize_mysql_manager(cls):
+        db_host = os.environ.get('DB_HOST')
+        db_user = os.environ.get('DB_USER')
+        db_password = os.environ.get('DB_PASSWORD')
+        db_port = os.environ.get('DB_PORT')
+        db_database = os.environ.get('DB_DATABASE')
+
+        # Check if environment variables are not None
+        assert db_host is not None, 'DB_HOST is not set'
+        assert db_user is not None, 'DB_USER is not set'
+        assert db_password is not None, 'DB_PASSWORD is not set'
+        assert db_port is not None, 'DB_PORT is not set'
+        assert db_database is not None, 'DB_DATABASE is not set'
+
+        # Connect to Mysql
+        cls.mysql_manager = MySQLManager(
+            host=db_host,
+            user=db_user,
+            password=db_password,
+            port=int(db_port),
+            database=db_database,
+        )
+        
     @classmethod
     def read_data_device(cls):
         """
